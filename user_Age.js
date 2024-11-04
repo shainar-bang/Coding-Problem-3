c47b1e6ba9ed8ac3853c66a3888d5d27beb4d423
shainar-bang <rabangshaina@gmail.com>
2024-11-04 18:40:52 +0800

    Create user_Age.js

---
+++ user_Age.js
@@ -0,0 +1,15 @@
+let age = "19";
+
+if (age < 5) {
+    console.log("You are a toddler.");
+} else if (age <= 12) {
+    console.log("You are a child.");
+} else if (age <= 19) {
+    console.log("You are a teenager.");
+} else if (age <= 35) {
+    console.log("You are a young adult.");
+} else if (age <= 60) {
+    console.log("You are middle-aged.");
+} else {
+    console.log("You are a senior.")
+}
